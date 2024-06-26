dd917a748622bb11d13bdbdb121dd2bd7cdf8e10
HP <HP@prabakar>
2024-06-26 22:42:15 +0530

    merged code

--- GitDemoProject/src/test/java/DemPackage/NewClassjava.java
+++ GitDemoProject/src/test/java/DemPackage/NewClassjava.java
@@ -4,7 +4,13 @@ public class NewClassjava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	// Anitha's code pushed	
+	    // Anitha's code pushed  pulled at 10 30	
+
+		// update sync with the master
+		
+		
+		
+		// new change 10 50
 
 	}
 
